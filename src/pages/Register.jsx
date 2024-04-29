bb3ae1e2d0a8fa8d866f188025efeacf735fd565
madiinemm <uraimovamadina179@gmail.com>
2024-04-29 11:05:53 +0500

    task bajarilmoqda

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,36 +1,34 @@
-import React from 'react';
-import {
-  MDBBtn,
-  MDBContainer,
-  MDBRow,
-  MDBCol,
-  MDBCard,
-  MDBCardBody,
-  MDBInput,
-  MDBIcon,
-  MDBCheckbox
-}
-from 'mdb-react-ui-kit';
+import React, { useState } from 'react';
+import { MDBBtn, MDBContainer, MDBRow, MDBCol, MDBCard, MDBCardBody, MDBInput, MDBIcon, MDBCheckbox } from 'mdb-react-ui-kit';
 
 function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Local storage'ga email va password ni saqlash
+    localStorage.setItem('email', email);
+    localStorage.setItem('password', password);
+
+    // Alertda muvaffaqiyatli ro'yxatdan o'tingiz degan yozuv chiqarish
+    alert('Muvaffaqiyatli ro\'yxatdan o\'tdingiz');
+  };
+
   return (
     <MDBContainer fluid>
-
       <MDBRow className='d-flex justify-content-center align-items-center h-100'>
         <MDBCol col='12'>
-
           <MDBCard className='bg-white my-5 mx-auto' style={{borderRadius: '1rem', maxWidth: '500px'}}>
             <MDBCardBody className='p-5 w-100 d-flex flex-column'>
-
               <h2 className="fw-bold mb-2 text-center">Sign in</h2>
               <p className="text-white-50 mb-3">Please enter your login and password!</p>
 
-              <MDBInput wrapperClass='mb-4 w-100' label='Email address' id='formControlLg' type='email' size="lg"/>
-              <MDBInput wrapperClass='mb-4 w-100' label='Password' id='formControlLg' type='password' size="lg"/>
+              <MDBInput wrapperClass='mb-4 w-100' label='Email address' id='formControlLg' type='email' size="lg" onChange={(e) => setEmail(e.target.value)} />
+              <MDBInput wrapperClass='mb-4 w-100' label='Password' id='formControlLg' type='password' size="lg" onChange={(e) => setPassword(e.target.value)} />
 
               <MDBCheckbox name='flexCheck' id='flexCheckDefault' className='mb-4' label='Remember password' />
 
-              <MDBBtn size='lg'>
+              <MDBBtn size='lg' onClick={handleLogin}>
                 Login
               </MDBBtn>
 
@@ -45,13 +43,10 @@ function App() {
                 <MDBIcon fab icon="facebook-f" className="mx-2"/>
                 Sign in with facebook
               </MDBBtn>
-
             </MDBCardBody>
           </MDBCard>
-
         </MDBCol>
       </MDBRow>
-
     </MDBContainer>
   );
 }
